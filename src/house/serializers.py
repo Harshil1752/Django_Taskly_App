f70f74132024e89d7618e92fc0ed3702bbbcda17
Harshil1752 <hlharshil175@gmail.com>
2023-05-15 13:15:26 +0530

    Third commit

--- src/house/serializers.py
+++ src/house/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from .models import House
 
 class HouseSerializer(serializers.ModelSerializer):
+
     members_count = serializers.IntegerField(read_only = True)
+    members = serializers.HyperlinkedRelatedField(read_only = True, many = True, view_name='profile-detail')
+    manager = serializers.HyperlinkedRelatedField(read_only = True, many = False, view_name='profile-detail')
 
     class Meta:
         model = House
         fields = ['url', 'id', 'image', 'name', 'created_on', 'manager', 'description', 
-                  'members_count', 'points',
-                  'completed_task_count', 'not_completed_task_count']
-        reads_only_fields = ['points', 'completed_task_count', 'not_completed_task_count']
+        'members_count', 'members', 'points','completed_task_count', 'not_completed_task_count']
+        read_only_fields = ['points', 'completed_task_count', 'not_completed_task_count']
